f105eee0c264f2dbc2f8ed8292a501e1fd05c03f
Leo Jones <17JonLeo@rwba.org.uk>
2023-10-04 22:21:51 +0100

    Got it back to a working state

--- Program.cs
+++ Program.cs
@@ -10,101 +10,80 @@ namespace Molecule_Namer
 {
     public class Program
     {
-       
+
         static void Main(string[] args)
         {
-            string Final = "";
-            startMenu(Final);
-            //string SF = "CH3CH2CH3";
-            string CSF = Final;
-            MoleculeProcesssor molProc = new MoleculeProcesssor();//creating an event og molecule process
-
-            Graph graph = molProc.processMolecule(CSF);
-            graph.PrintMatrix();
-            Console.ReadLine();
+            startMenu();
         }
-        static void startMenu(string Final)
+        static void startMenu()
         {
-            bool valid;
+            bool valid = true;
             do
             {
-                valid = true;   
+                string CSF;
                 Console.WriteLine("what is your choice? \n type '1' to input a custom structural formula: \n type '2' to list all molecules stored: ");
-                string choice = Console.ReadLine();
+                string? choice = Console.ReadLine();
+
                 switch (choice)
                 {
                     case "1":
-                        Console.WriteLine("1");
-                        valid = false;
-                        SFInput(Final);
-                        Console.WriteLine("CHICK1");
+                        CSF = SFInput();
+                        MoleculeProcesssor molProc = new MoleculeProcesssor();  //creating an event og molecule process
+
+                        Graph graph = molProc.processMolecule(CSF);
+                        graph.PrintMatrix();
+
                         valid = false;
                         break;
                     case "2":
-                        Console.WriteLine("2");
+                        Console.WriteLine("Got 2");
                         break;
                     default:
                         Console.WriteLine("na ah");
                         break;
-                    
-
                 }
 
-            }
-            while (valid == true);
+            } while (valid);
         }
-        private static string SFInput(string Final)
+        private static string SFInput()
         {
-            bool valid;
-            Final = "";
-            string First;
+            bool valid = true;
+            string final;
+            string? first;
             do
             {
-                valid = true;
                 Console.WriteLine("what is your structural formula? ");
-                First = Console.ReadLine();//gets an input for the Structural formula
-                
-                SFconversion(First, Final, valid);
+
+                first = Console.ReadLine();  //gets an input for the Structural formula
+                final = SFconversion(first);
                 valid = false;
 
+            } while (valid);
 
-            } while
-            (valid == true);
-            
-
-            
+            return final;
         }
-        static void SFconversion(string First, string Final, bool valid) {
-            
-            
+
+        static string SFconversion(string First)
+        {
             Console.WriteLine("test");
-            for (int i = 0; i < First.Length; i++){
-                if (First[i] == 'C' || First[i] == '('|| First[i] == ')')
+            string final = "";
+            for (int i = 0; i < First.Length; i++)
+            {
+                if (First[i] == 'C' || First[i] == '(' || First[i] == ')')
                 {
-                    
-                    Final = Final + First[i];
-                    Console.WriteLine(Final);
-                    
+
+                    final = final + First[i];
+                    Console.WriteLine(final);
+
 
                     //char[] nameArray = {'A', 'l', 'i', 'c', 'e'};
                     //string name = new string(nameArray);
-                    
+
                 }
             }
-            Console.WriteLine("done");
-            ValidCheck(Final, valid);
-            Console.WriteLine("chick2");
-            
-             
+            return final;
+            //Console.WriteLine("chick2");
         }
-        
-        static void ValidCheck(string Final, bool valid) { 
-            valid = false;
-            Console.WriteLine("chick3");
-        }
-
-
-
     }
 }
-    
+
