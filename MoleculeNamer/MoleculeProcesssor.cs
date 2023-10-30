a563facfa97db0ed094579e3a355667acddd8b4d
ChonkyDonkey <leo.starquality@gmail.com>
2023-10-30 11:11:14 +0000

    added more test cases

--- MoleculeNamer/MoleculeProcesssor.cs
+++ MoleculeNamer/MoleculeProcesssor.cs
@@ -12,7 +12,7 @@ namespace MoleculeNamer
         Dictionary<string, Node> molecule = new Dictionary<string, Node>(); // this makes a dictionary of all the nodes
 
         public bool validateString(string CSF){
-            bool valid = false;
+            bool valid = True;
             return valid;
         }
         private void findArc(string CSF, Dictionary<string, Node> molecule)
--- MoleculeNamerTest/UnitTest1.cs
+++ MoleculeNamerTest/UnitTest1.cs
@@ -40,5 +40,81 @@ namespace MoleculeNamer.UnitTests
             bool result = _moleculeProcessor.validateString("C");
             Assert.IsTrue(result, "\'C\' should pass test");
         }
+        
+        [TestMethod]
+        public void MoleculeProcesssor_testBasicMolecule()
+        {
+            bool result = _moleculeProcessor.validateString("C)");
+            Assert.IsFalse(result, "\'C)\' should fail test");
+        }
+
+        [TestMethod]
+        public void MoleculeProcesssor_testBasicMolecule()
+        {
+            bool result = _moleculeProcessor.validateString("C(");
+            Assert.IsFalse(result, "\'C(\' should fail test");
+        }
+
+        [TestMethod]
+        public void MoleculeProcesssor_testBasicMolecule()
+        {
+            bool result = _moleculeProcessor.validateString("()");
+            Assert.IsFalse(result, "\'()\' should Fail test");
+        }
+
+        [TestMethod]
+        public void MoleculeProcesssor_testBasicMolecule()
+        {
+            bool result = _moleculeProcessor.validateString("C()");
+            Assert.IsFalse(result, "\'C()\' should Fail test");
+        }
+       
+        [TestMethod]
+        public void MoleculeProcesssor_testBasicMolecule()
+        {
+            bool result = _moleculeProcessor.validateString("C()C");
+            Assert.IsFalse(result, "\'C()C\' should fail test");
+        }
+        [TestMethod]
+        public void MoleculeProcesssor_testBasicMolecule()
+        {
+            bool result = _moleculeProcessor.validateString("C(C)");
+            Assert.IsFalse(result, "\'C(C)\' should fail test");
+        }
+        [TestMethod]
+        public void MoleculeProcesssor_testBasicMolecule()
+        {
+            bool result = _moleculeProcessor.validateString("C(C)C");
+            Assert.IsTrue(result, "\'C(C)C\' should pass test");
+        }
+
+        [TestMethod]
+        public void MoleculeProcesssor_testBasicMolecule()
+        {
+            bool result = _moleculeProcessor.validateString("()C");
+            Assert.IsFalse(result, "\'()C\' should Fail test");
+        }
+        
+        [TestMethod]
+        public void MoleculeProcesssor_testBasicMolecule()
+        {
+            bool result = _moleculeProcessor.validateString("C((C))C");
+            Assert.IsFalse(result, "\'C\' should Fail test");
+        }
+        
+        [TestMethod]
+        public void MoleculeProcesssor_testBasicMolecule()
+        {
+            bool result = _moleculeProcessor.validateString("C(C(C))C");
+            Assert.IsPass(result, "\'C(C(C))C\' should pass test");
+        }
+        
+        [TestMethod]
+        public void MoleculeProcesssor_testBasicMolecule()
+        {
+            bool result = _moleculeProcessor.validateString("C(C(C)C");
+            Assert.IsFalse(result, "\'C(C(C)C\' should Fail test");
+        }
+
     }
 }
